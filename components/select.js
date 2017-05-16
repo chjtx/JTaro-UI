013b79bde04fff1279340e530e57d8c052d4a1b1
BarZu <1419328765@qq.com>
2017-05-16 17:18:56 +0800

    完成 j-select 的单选

--- components/select.js
+++ components/select.js
@@ -1,5 +1,6 @@
 /* global Vue JRoll */
 import './mask.js'
+import './select.css'
 import html from './select.html'
 
 (function (v, Jro) {
@@ -8,7 +9,20 @@ import html from './select.html'
     props: {
       value: String
     },
-    template: '<li :value="value"><slot></slot></li>'
+    template: '<li class="j-click-button" :class="css" :value="value" @click="selectValue"><slot></slot></li>',
+    computed: {
+      css: function () {
+        return {
+          'j-bg-primary': String(this.value) === this.$parent.value,
+          'j-font-primary': String(this.value) === this.$parent.value
+        }
+      }
+    },
+    methods: {
+      selectValue: function () {
+        this.$parent.setValue(this.value, this.$el.innerText)
+      }
+    }
   })
 
   // 选择器
@@ -18,7 +32,7 @@ import html from './select.html'
         type: Boolean,
         default: false
       },
-      'value': String,
+      'value': [String, Array],
       'show': {
         type: Boolean,
         default: false
@@ -31,14 +45,25 @@ import html from './select.html'
         toShow: this.show
       }
     },
-    mounted: function () {
-      var children = this.$refs.options.children
-      for (var i = 0, l = children.length; i < l; i++) {
-        if (this.value === String(children[i].value)) {
-          this.inputValue = children[i].innerText
+    computed: {
+      cssObject: function () {
+        return {
+          'j-select-show': this.toShow
         }
       }
+    },
+    mounted: function () {
+      var showScrollBar = this.changeValue() > 6
       document.body.appendChild(this.$refs.mask)
+      this.jroll = new Jro(this.$refs.jroll, { scrollBarY: showScrollBar, scrollBarFade: true })
+    },
+    updated: function () {
+      if (this.jroll) this.jroll.refresh()
+    },
+    watch: {
+      value: function () {
+        this.changeValue()
+      }
     },
     methods: {
       showOptions: function () {
@@ -53,7 +78,21 @@ import html from './select.html'
         this.toShow = false
         setTimeout(function () {
           me.$refs.mask.style.display = 'none'
-        }, 250)
+        }, 350)
+      },
+      changeValue: function () {
+        var children = this.$refs.options.children
+        for (var i = 0, l = children.length; i < l; i++) {
+          if (this.value === String(children[i].value)) {
+            this.inputValue = children[i].innerText
+          }
+        }
+        return l
+      },
+      setValue: function (val, text) {
+        this.inputValue = text
+        this.$emit('input', val)
+        this.hideOptions()
       }
     }
   })

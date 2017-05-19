3b08940d9507440b567b9aba27b9d96a11b2bbc6
BarZu <1419328765@qq.com>
2017-05-19 16:43:39 +0800

    更新build和依赖

--- build.js
+++ build.js
@@ -1,11 +1,15 @@
 var jtaroBundle = require('jtaro-bundle')
 var uglify = require('rollup-plugin-uglify')
+var alias = require('rollup-plugin-paths')
 
 jtaroBundle.bundle({
   origin: 'index.html',  // 开发目录的index.html
   target: 'build/index.html',  // 生产目录的index.html
   copies: ['src/'],  // 直接拷贝的文件（夹）
-  rollupPlugins: [uglify()]
+  rollupPlugins: [alias({
+    'node@': './node_modules/' // 以入口文件所在路径为基准
+  }), uglify()],
+  sourceMap: false
 })
 
 console.log('build complete!')
